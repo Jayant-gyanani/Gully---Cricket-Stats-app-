17264b111ca3b52317d0022efbff35200ec52cef
Jayant Gyanani <163506858+Jayant-gyanani@users.noreply.github.com>
2026-01-21 01:04:45 +0530

    Update constants.ts
    removed the unwanted players and updated the password of admin and umpire

--- constants.ts
+++ constants.ts
@@ -2,13 +2,8 @@
 import { Role, User } from './types';
 
 export const INITIAL_USERS: User[] = [
-  { id: 'admin1', username: 'admin', password: '123', role: Role.ADMIN },
-  { id: 'umpire1', username: 'umpire', password: '123', role: Role.UMPIRE },
-  { id: 'player1', username: 'rohit', password: '123', role: Role.PLAYER },
-  { id: 'player2', username: 'virat', password: '123', role: Role.PLAYER },
-  { id: 'player3', username: 'dhoni', password: '123', role: Role.PLAYER },
-  { id: 'player4', username: 'hardik', password: '123', role: Role.PLAYER },
-  { id: 'player5', username: 'bumrah', password: '123', role: Role.PLAYER },
+  { id: 'admin1', username: 'admin', password: '190805', role: Role.ADMIN },
+  { id: 'umpire1', username: 'umpire', password: '321', role: Role.UMPIRE },
 ];
 
 export const COLORS = {
